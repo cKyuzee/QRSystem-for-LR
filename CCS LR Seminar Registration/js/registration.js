8f288459cb130d8402b355248a7571649a12f55d
JohnClark27 <43731929+JohnClark27@users.noreply.github.com>
2019-01-31 16:59:18 +0800

    Final Registration UI
    Registration UI is Done!

--- CCS LR Seminar Registration/js/registration.js
+++ CCS LR Seminar Registration/js/registration.js
@@ -1,9 +1,34 @@
 jQuery(document).ready(function($) {
-  var qrcode = new QRCode(document.getElementById("qrcode"), {
+  let qrcode = new QRCode(document.getElementById("qrcode"), {
     width: 200,
     height: 200
   });
 
+  $('#select-delegateType').change(function (){
+    let type = "";
+    $('#select-delegateType option:selected').each(function (){
+      type = $(this).text();
+    });
+
+    switch (type) {
+      case "NEU Student":
+        $('.student-information').show(500);
+        $('.alumni-information').hide(500);
+        $('.faculty-information').hide(500);
+        break;
+      case "NEU Alumni":
+        $('.alumni-information').show(500);
+        $('.student-information').hide(500);
+        $('.faculty-information').hide(500);
+        break;
+      case "NEU Faculty":
+        $('.student-information').hide(500);
+        $('.alumni-information').hide(500);
+        $('.faculty-information').show(500);
+        break;
+      default:
+    }
+  });
 
   $('#btnRegisterSubmit').click(function() {
     $('#modal-showQRcode').modal().show();
